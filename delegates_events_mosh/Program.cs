17060fa6aa2e649fb6e826a21c3647126c76215b
Jørund Jenssen Berg <jjberg@gmail.com>
2023-07-05 15:01:24 +0200

    Endringer frem til 17.53

--- delegates_events_mosh/Program.cs
+++ delegates_events_mosh/Program.cs
@@ -7,9 +7,30 @@ namespace EventsAndDelegates
         static void Main(string[] args)
         {
             var video = new Video() { Title = "Gladiatoren" };
-            var videoEncoder = new VideoEncoder();
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+
+            // Her ordner vi subscription for alle subscribers
+            // format er publisher.event 
+            // vi registerer en handler for the event
+            // handleren er metoden OnVideoEncoded i klassen MailService
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
 
             videoEncoder.Encode(video);
+        }
+    }
+
+    // under lager vi en del subscribere som følger med på endringer
+    // i VideoEncoder klassen
+
+    public class MailService
+    {
+        // Metoden under følger samme oppsett som "signaturen" til VideoEncoder klassen
+        // "Egentlig" skal metoden sende Epost
+        // Vi simulerer ved å skrive til konsollen
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("MailService: Sending an email...");
         }
     }
 }
--- delegates_events_mosh/VideoEncoder.cs
+++ delegates_events_mosh/VideoEncoder.cs
@@ -5,34 +5,42 @@ namespace EventsAndDelegates
 {
     public class VideoEncoder
     {
-        // 3 steg
-        // 1 - Definer en delegate (kontrakt mellom avsender, altså denne klassen
-        // , og alle subscribere som lytter til endringer i denne klassen )
-        // 2 - Definer en event basert på delegaten over
-        // 3 - Raise/publish the event, dvs lag method som publiserer endringer
-        // og kall denne funksjonen
 
-        // Steg 1
+        // Steg 1 - Definer en delegate (kontrakt mellom avsender, altså denne klassen,
+-       // og alle subscribere som lytter til endringer i denne klassen)
         // Parameteren objekt er en instans av VideoEncoder klassen
         // Parameteren  EventArgs er eventuell tillegsdata om eventen vi ønsker å legge til
         // Navnet følger konvensjonen navn-på-event + EventHandler
-        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+
+        // tror dette kalles "signature", her definerer vi "signature"
+         public delegate void VideoEncodedEventHandler(object source, EventArgs args); 
 
         // Steg 2
+        // Definer en event basert på delegaten over
         public event VideoEncodedEventHandler VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
+
             // Steg 3 (delsteg)
+            // kall på funksjon som publiserer endringer i klassen, til alle som lytter på (når alt over er ferdig)
+            // i stedet for mange linjer inni denne funskjonen der jeg kaller på meldinger, telefon, pc osv manuelt
+            OnVideoEncoded();
         }
 
-        // Steg 3 (delsteg: lag method som publiserer endringer til alle groupies som abonnerer
+        // Steg 3: Raise/publish the event, dvs lag method som publiserer endringer til ALLE som lytter på
+        //(delsteg: lag method som publiserer endringer til alle groupies som abonnerer)
         // protected virtual void er konvensjon
         // Navnekonvensjon også med On + navn-på-event
         protected virtual void OnVideoEncoded() 
         {
-
+            if (VideoEncoded != null) // stemmer denne betyr det at det finnes subscribere
+            {
+                // this viser til denne instansen av klassen, EventArgs.Empty lager en tom EventArgs
+                // mao, man legger ikke med tilleggsdata
+                VideoEncoded(this, EventArgs.Empty);
+            }
         }
     }
 }
