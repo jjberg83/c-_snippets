ab1326742a7afc7ab57fc7baf1ebc6ca9ac97e24
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-02 08:35:28 +0200

    Fullførte nameSwapProject challenge fra Edabit

--- nameSwapProject/Program.cs
+++ nameSwapProject/Program.cs
@@ -4,14 +4,13 @@ class Program
 {
     public static void NameShuffle(string str)
     {
-        // return $">>    {str}    >>";
-        char[] ch = str.ToCharArray();
-        Console.WriteLine($"Arrayen er nå {ch}");
+        string[] ch = str.Split();
+        Console.WriteLine($"Navnet er nå reversert til {ch[1]} {ch[0]}");
     }
 
     static void Main(string[] args)
     {
-        NameShuffle("KanonKode");
+        NameShuffle("Kanon Kode");
     }
 }
 
