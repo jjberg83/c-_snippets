bcd7863d4b13bc6efe3a3ecf8084b8008acdb358
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-03 11:00:44 +0200

    La til prosjekt som handlet om checked (forhindrer overflow)

---
+++ C_Mosh/1/OverflowProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OverflowProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Checked gjør at mittNummer ikke starter om igjen på 0
+        // Mosh bruker det ikke, han bare endrer typen til
+        // datatype som er litt større
+        checked
+        {
+            try
+            {
+                byte mittNummer = 255;
+                mittNummer += 1;
+                Console.WriteLine(mittNummer);
+            }
+            catch
+            {
+                Console.WriteLine("Noe gikk galt her...");
+            }
+        }
+    }
+}
+
