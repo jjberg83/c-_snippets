8e9f75ef72b0b63c358f41beb39f5732c248dd30
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-04 09:36:51 +0200

    Fullførte Operators Demo video

---
+++ C_Mosh/1/OperatorsDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OperatorsDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = 10;
+        var b = 3;
+        Console.WriteLine($"a / b = {a/b}");
+        Console.WriteLine($"a / b = {(float)a / (float)b}");
+
+        var c = 1;
+        var d = 2;
+        var e = 3;
+        Console.WriteLine(!(c != d));
+        Console.WriteLine(!(e > c || e == d));
+    }
+}
+
