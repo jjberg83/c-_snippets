8879794ae6a621c62e88cd2681a1e32b6c803793
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-04 09:21:57 +0200

    La til Operators prosjekt for Operators videoen

---
+++ C_Mosh/1/Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Operators;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = a++;
+        Console.WriteLine($"a: {a}, b: {b}");
+
+        int c = 1;
+        int d = ++c;
+        Console.WriteLine($"c: {c}, d: {d}");
+
+        bool sant = true;
+        bool vel = true;
+        bool eller = false;
+
+        //AND og OR er doble
+        Console.WriteLine($"{sant && vel}");
+        Console.WriteLine($"{sant && eller}");
+        Console.WriteLine($"{sant || eller}");
+
+        //bitwise operators er enkle
+        byte nummerEn = 1;
+        byte nummerTo = 2;
+        byte nummerTre = 3;
+        Console.WriteLine($"{nummerEn & nummerTre}");
+        Console.WriteLine($"{nummerEn | nummerTre}");
+
+    }
+}
+
