2c625c84a988fa86bfbbf2d94c7fb934adc73362
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-07 15:13:08 +0200

    Demo: Arrays Video

--- C_Mosh/1/NonPrimitiveProject/Program.cs
+++ C_Mosh/1/NonPrimitiveProject/Program.cs
@@ -43,5 +43,27 @@ class Program
         //myArray[0] = 1;
         //Console.WriteLine(myArray[0]);
 
+        //////////////////////////////////
+        /// DEMO: ARRAYS Video
+        //////////////////////////////////
+
+        //var numbers = new int[3];
+        //numbers[0] = 1;
+        //Console.WriteLine(numbers[0]);
+        //Console.WriteLine(numbers[1]);
+        //Console.WriteLine(numbers[2]);
+
+        //var flags = new bool[4];
+        //flags[0] = true;
+        //Console.WriteLine(flags[0]);
+        //Console.WriteLine(flags[1]);
+        //Console.WriteLine(flags[2]);
+        //Console.WriteLine(flags[3]);
+
+        //var familienMin = new string[4] { "Marlene", "Elly", "Lori", "Jørund" };
+        //Console.WriteLine(familienMin[0]);
+        //Console.WriteLine(familienMin[1]);
+        //Console.WriteLine(familienMin[2]);
+        //Console.WriteLine(familienMin[3]);
     }
 }
