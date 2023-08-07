b299b7543fb80d14be87c0834fc1c23b165f3d40
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-07 14:54:21 +0200

    i++ eller ++1

---
+++ C_Mosh/1/plusPlusProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace plusPlusProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b, c;
+        Console.WriteLine($"a:{a}");
+        Console.WriteLine($"b=a++ blir {b=a++}, a er {a}");
+        Console.WriteLine($"a:{a}");
+        Console.WriteLine($"c=++a blir {c = ++a}, a er {a}");
+        int[] myArr = new int[5];
+        //Console.WriteLine($"myArr:{myArr}");
+        foreach(int tall in myArr)
+        {
+            Console.Write(tall);
+        }
+    }
+}
+
