26744d62d851c29cdd5519a9e34fff9115904214
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-08 10:55:40 +0200

    Fullførte enum test, men ikke helt fornøyd

--- C_Mosh/1/enumProject/Program.cs
+++ C_Mosh/1/enumProject/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Enums brukes når man har flere relaterte konstanter
+        // I stedet for dette
+
+        //const int RegularAirMail = 1;
+        //const int RegisteredAirMail = 2;
+        //const int Express = 3;
+
+        //Console.WriteLine(RegularAirMail);
+        //Console.WriteLine(RegisteredAirMail);
+        //Console.WriteLine(Express);
+        var method = ShippingMethod.RegularAirMail;
+        Console.WriteLine($"method: {method}");
     }
 }
 
---
+++ C_Mosh/1/enumProject/ShippingMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace enumProject
+{
+    // Samle heller alle konstanter over i en enum
+    public enum ShippingMethod : byte
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+}
