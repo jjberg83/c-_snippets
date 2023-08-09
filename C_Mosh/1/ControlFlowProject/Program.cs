7e53523c22c973845d941af0b98b7f5a5d41a2e5
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-09 09:54:14 +0200

    Første for ControlFlow

---
+++ C_Mosh/1/ControlFlowProject/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ControlFlowProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /////////////////////////////
+        /// DEMO IF/ELSE, SWITCH
+        /////////////////////////////
+
+        //var hour = 10;
+
+        //// Har man kun EN linje under if, else if og else
+        //// trenger man ikke curly braces
+        //if (hour > 0 && hour < 12) 
+        //    Console.WriteLine("Det er morgen.");
+        //else if (hour >= 12 && hour < 18)
+        //    Console.WriteLine("Det er ettermiddag.");
+        //else
+        //    Console.WriteLine("Det er kveld.");
+
+        bool isGoldMember = true;
+        float price;
+        //if (isGoldMember)
+        //    price = 19.95f;
+        //else
+        //    price = 29.95f;
+
+        // med conditional operator
+        price = isGoldMember ? 19.96f : 29.96f;
+
+        Console.WriteLine($"Prisen er {price}");
+
+        var sesong = Season.Summer;
+
+        //Console.WriteLine(sesong);
+        //Console.WriteLine((int)sesong);
+
+        switch (sesong)
+        {
+            case Season.Spring:
+                Console.WriteLine("Det er vår i luften!");
+                break;
+            case Season.Summer:
+                Console.WriteLine("Sommer og sol!");
+                break;
+            case Season.Autumn:
+                Console.WriteLine("Høsten kommer, takk for sommer!");
+                break;
+            case Season.Winter:
+                Console.WriteLine("Det snør, det snør...");
+                break;
+
+        }
+
+
+    }
+}
+
---
+++ C_Mosh/1/ControlFlowProject/Season.cs
@@ -0,0 +1,16 @@
+﻿namespace ControlFlowProject
+{
+    // Trenger ikke legge verdier til her
+    // siden vi ikke jobber med en database
+    // starter da med verdiene 0 og oppover
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+}
+
+
