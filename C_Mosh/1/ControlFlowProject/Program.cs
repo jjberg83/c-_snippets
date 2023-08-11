9a10b0654489d2919ee957a1237a6317c98567a8
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-11 11:23:43 +0200

    For loops demo

--- C_Mosh/1/ControlFlowProject/Program.cs
+++ C_Mosh/1/ControlFlowProject/Program.cs
@@ -19,40 +19,58 @@ class Program
         //else
         //    Console.WriteLine("Det er kveld.");
 
-        bool isGoldMember = true;
-        float price;
+        //bool isGoldMember = true;
+        //float price;
         //if (isGoldMember)
         //    price = 19.95f;
         //else
         //    price = 29.95f;
 
         // med conditional operator
-        price = isGoldMember ? 19.96f : 29.96f;
+        //price = isGoldMember ? 19.96f : 29.96f;
 
-        Console.WriteLine($"Prisen er {price}");
+        //Console.WriteLine($"Prisen er {price}");
 
-        var sesong = Season.Summer;
+        //var sesong = Season.Summer;
 
         //Console.WriteLine(sesong);
         //Console.WriteLine((int)sesong);
 
-        switch (sesong)
-        {
-            case Season.Spring:
-                Console.WriteLine("Det er vår i luften!");
-                break;
-            case Season.Summer:
-                Console.WriteLine("Sommer og sol!");
-                break;
-            case Season.Autumn:
-                Console.WriteLine("Høsten kommer, takk for sommer!");
-                break;
-            case Season.Winter:
-                Console.WriteLine("Det snør, det snør...");
-                break;
+        //switch (sesong)
+        //{
+        //    case Season.Spring:
+        //        Console.WriteLine("Det er vår i luften!");
+        //        break;
+        //    case Season.Summer:
+        //        Console.WriteLine("Sommer og sol!");
+        //        break;
+        //    case Season.Autumn:
+        //        Console.WriteLine("Høsten kommer, takk for sommer!");
+        //        break;
+        //    case Season.Winter:
+        //        Console.WriteLine("Det snør, det snør...");
+        //        break;
 
-        }
+        //}
 
+        /////////////////////////////
+        /// DEMO FOR LOOPS
+        /////////////////////////////
+
+        //for (var i = 1; i <= 10; i++)
+        //{
+        //    if (i%2==0)
+        //        Console.WriteLine(i);
+        //}
+        //for (var i = 10; i > 0; i--)
+        //{
+        //    if (i%2==0)
+        //        Console.WriteLine(i);
+        //}
+
+        /////////////////////////////
+        /// DEMO FOREACH LOOPS
+        /////////////////////////////
 
     }
 }
