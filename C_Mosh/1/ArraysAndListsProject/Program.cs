92cf3dc899e0acefc50a7323f966b64a20286571
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-17 10:11:32 +0200

    Startet seksjonen Arrays and lists på Mosh modul 1

---
+++ C_Mosh/1/ArraysAndListsProject/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ArraysAndListsProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ////////////////////
+        ///  Arrays video
+        ////////////////////
+        ///
+
+        // rectangular array
+        //var matrix = new int[3, 5]; // altså 3 rows, 5 columns
+        //var matrix = new int[3, 5]
+        //{
+        //    {1,2,3,4,5 },
+        //    {6,7,8,9,10 },
+        //    {11,12,13,14,15 }
+        //};
+        //var element = matrix[0,0];
+        //Console.WriteLine(element);
+
+        // jagged array
+        //var matrix = new int[3][];
+        //matrix[0] = new int[3] {1,2,3}; // rad 1 skal ha array med 3 elementer
+        //matrix[1] = new int[5] {4,5,6,7,8}; // rad 2 skal ha array med 5 elementer
+        //matrix[0] = new int[4] {9,10,11,12}; // rad 3 skal ha array med 4 elementer
+        //Console.WriteLine($"{matrix[1][2]}");
+    }
+}
+
