bb5047005a06b2eb7dc028009a621d5c90ce7a6d
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-30 13:46:45 +0200

    Startet prosjekt 2 i dependency Injection læringen min

---
+++ dependencyInjection/ConsoleDI.Example/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Tutorial finnes her:
+// https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-usage
+Console.WriteLine("Joda");
