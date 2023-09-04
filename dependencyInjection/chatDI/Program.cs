2877df13e2f39bb8e2e594b0afbdb8a4f82b2898
Jørund Jenssen Berg <jjberg@gmail.com>
2023-09-04 12:03:27 +0200

    Ser ikke hvordan UserService blir uberørt ift å ikke bruke dependency injection

--- dependencyInjection/chatDI/Program.cs
+++ dependencyInjection/chatDI/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Without dependency injection
+
+UserService jjberg = new UserService(); // injecter ingenting her
+jjberg.RegisterUser("jjberg83", "jjberg@gmailcom");
+
+class EmailService
+{
+    public void SendEmail(string to, string message)
+    {
+        // code to send an email
+        Console.WriteLine($"ToMail: {to}, TheMessage: {message}");
+    }
+}
+
+class UserService
+{
+    private EmailService emailService = new EmailService();
+
+    public void RegisterUser(string username, string email)
+    {
+        // register the user and send a welcome email
+        emailService.SendEmail(email, "Welcome to our website");
+    }
+}
+
+
+// with dependency injection
+
+// UserService jjberg = new UserService(new EmailService());
+// jjberg.RegisterUser("jjberg83", "jjberg@gmailcom");
+
+// interface IEmailService
+// {
+//     void SendEmail(string to, string message);
+// }
+
+// class EmailService : IEmailService
+// {
+//     public void SendEmail(string to, string message)
+//     {
+//         // code to send email
+//         Console.WriteLine($"ToMail: {to}, TheMessage: {message}");
+//     }
+// }
+
+// class UserService
+// {
+//     private readonly IEmailService emailService;
+
+//     public UserService(IEmailService emailService) // konstruktør krever at instans må injectes
+//     {
+//         this.emailService = emailService;
+//     }
+
+//     public void RegisterUser(string username, string email)
+//     {
+//         // register the user and send a welcome email
+//         emailService.SendEmail(email, "Welcome to our website");
+//     }
+// }
