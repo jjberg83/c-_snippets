8760e2a8476f299f4109fcc0067424e7049c5b22
Jørund Jenssen Berg <jjberg@gmail.com>
2023-09-06 17:37:03 +0200

    Fullførte mitt eget eksperiment med Dependency Injection, men må jobbe mer med å forstå ServiceCollection containeren

---
+++ dependencyInjection/DIEssens/medDI/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+                      .AddSingleton<IKoneKommunikasjon, SkrikTilKone>()
+                      .AddSingleton<Mann>()
+                      .BuildServiceProvider();
+
+var jjberg = serviceProvider.GetRequiredService<Mann>();
+
+
+jjberg.SiNoe();
+Console.WriteLine();
+
+
+class SkrikTilKone : IKoneKommunikasjon
+{
+    public void Respons()
+    {
+        Console.WriteLine("Faen altså, ta oppvasken");
+    }
+}
+
+class KosePratTilKone : IKoneKommunikasjon
+{
+    public void Respons()
+    {
+        Console.WriteLine("Du ser så flott ut i dag");
+    }
+}
+
+public interface IKoneKommunikasjon
+{
+    void Respons();
+}
+
+class Mann
+{
+    private readonly IKoneKommunikasjon _koneKommunikasjon;
+
+    public Mann(IKoneKommunikasjon koneKommunikasjon) // konstruktør krever at instans må injectes
+    {
+        _koneKommunikasjon = koneKommunikasjon;
+    }
+
+    public void SiNoe()
+    {
+        _koneKommunikasjon.Respons();
+    }
+}
+
---
+++ dependencyInjection/DIEssens/utenDI/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var enMann = new Mann();
+enMann.SiNoe();
+
+
+
+class SkrikTilKone
+{
+    public void Respons()
+    {
+        Console.WriteLine("Faen altså, ta oppvasken");
+    }
+}
+
+class KosePratTilKone
+{
+    public void Respons()
+    {
+        Console.WriteLine("Du ser så flott ut i dag");
+    }
+}
+
+class Mann
+{
+    KosePratTilKone kommunikasjon =  new KosePratTilKone();
+    // SkrikTilKone kommunikasjon = new SkrikTilKone();
+
+
+    public void SiNoe()
+    {
+        kommunikasjon.Respons();
+    }
+}
+
